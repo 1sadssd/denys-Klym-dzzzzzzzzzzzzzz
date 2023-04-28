aaa62f0e4f0ec73a1f5b680521207bdf85f461c8
1sadssd <den20062345@gmail.com>
2023-04-28 23:09:30 +0300

    Добавьте файлы проекта.

---
+++ denys Klym dzzzzzzzzzzzzzz/denys Klym dzzzzzzzzzzzzzz.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include<fstream>
+#include<string>
+#include <Windows.h>
+using namespace std;
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int rahynok = 0;
+    char bykva;
+    ifstream file("text.txt");
+    string word;
+    
+    cout << "Введіть букву : ";
+    cin >> bykva;
+
+    while (file >> word) 
+    {
+        if (word[0] == bykva) 
+        {
+            rahynok ++;
+        }
+    }
+    file.close();
+
+    cout << "К-сть слів що починаються на літеру  " << bykva << ": " << rahynok << endl;
+
+    return 0;
+
+
+
+}
